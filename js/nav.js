fe5b509d6923551c58b7caf9f60e1696e8a644f6
bigyihsuan <bigyihsuan@gmail.com>
2022-04-18 15:42:25 -0400

    adjust movement times for navbar

--- js/nav.js
+++ js/nav.js
@@ -2,6 +2,11 @@ var navtab_active = null;
 var anchor_queued = null;
 var click_enabled = true;
 
+const ANCHOR_HIDE_DELAY = 75;
+const ANCHOR_FADE_IN = 300;
+const ANCHOR_SLIDE_TIME = 385;
+
+
 function navtab_id(e) {
     if (click_enabled) {
         var anchor_id = e.toString();
@@ -9,33 +14,33 @@ function navtab_id(e) {
     };
 };
 
-$(document).ready(function () {
-    $("html").click(function (e) {
+$(document).ready(function() {
+    $("html").click(function(e) {
         if (click_enabled) {
             click_enabled = false;
             if (navtab_active == null && anchor_queued != null) {
-                $(anchor_queued + " a").hide().delay(150).fadeIn(600);
-                $(anchor_queued).slideDown(750, function() {
+                $(anchor_queued + " a").hide().delay(ANCHOR_HIDE_DELAY).fadeIn(ANCHOR_FADE_IN);
+                $(anchor_queued).slideDown(ANCHOR_SLIDE_TIME, function() {
                     navtab_active = anchor_queued;
                     anchor_queued = null;
                     click_enabled = true;
                 });
             } else if (navtab_active != null &&
-            anchor_queued == null &&
-            !(e.target.nodeName == "A" ||
-            e.target.nodeName == "IMG")) {
-                $(navtab_active + " a").show().fadeOut(400);
-                $(navtab_active).slideUp(550, function() {
+                anchor_queued == null &&
+                !(e.target.nodeName == "A" ||
+                    e.target.nodeName == "IMG")) {
+                $(navtab_active + " a").show().fadeOut(ANCHOR_FADE_IN);
+                $(navtab_active).slideUp(ANCHOR_SLIDE_TIME, function() {
                     navtab_active = null;
                     click_enabled = true;
-                });   
+                });
             } else if (anchor_queued != null &&
-            anchor_queued != navtab_active) {
-                $(navtab_active + " a").show().fadeOut(400);
-                $(navtab_active).slideUp(550, function() {
+                anchor_queued != navtab_active) {
+                $(navtab_active + " a").show().fadeOut(ANCHOR_FADE_IN);
+                $(navtab_active).slideUp(ANCHOR_SLIDE_TIME, function() {
                     navtab_active = null;
-                    $(anchor_queued + " a").hide().delay(150).fadeIn(600);
-                    $(anchor_queued).slideDown(750, function() {
+                    $(anchor_queued + " a").hide().delay(ANCHOR_HIDE_DELAY).fadeIn(ANCHOR_FADE_IN);
+                    $(anchor_queued).slideDown(ANCHOR_SLIDE_TIME, function() {
                         navtab_active = anchor_queued;
                         anchor_queued = null;
                         click_enabled = true;
