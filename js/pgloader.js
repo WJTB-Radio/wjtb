a1a22447449207dda6276be36b7b098065752824
Al Simpson <alfredhsimpson@gmail.com>
2022-09-01 18:38:38 -0400

    Major overhaul
    This represents the first in a major overhaul of the site.
    - AJAX implementation
    - Mobile first design
    - Branding set, but not firm
    - Kept old site on the back end (need to check quota)
    
    Still need:
    -- css edits
    -- better branding
    -- a lot of "pages" are empty, still need to get those squared

---
+++ js/pgloader.js
@@ -0,0 +1,104 @@
+function loadHome() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function () {
+        document.getElementById("output").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "./pages/home.txt")
+    xhttp.send();
+}
+
+// Programming category
+function loadShows() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function () {
+        document.getElementById("output").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "./pages/shows.txt")
+    xhttp.send();
+}
+
+function loadVariety() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function () {
+        document.getElementById("output").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "./pages/variety.txt")
+    xhttp.send();
+}
+
+function loadJoin() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        document.getElementById("output").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "./pages/join.txt")
+    xhttp.send();
+}
+
+// Events  category
+
+function loadUpcoming() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function () {
+        document.getElementById("output").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "./pages/upcoming.txt")
+    xhttp.send();
+}
+
+function loadPast() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function () {
+        document.getElementById("output").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "./pages/past.txt")
+    xhttp.send();
+}
+
+function loadSoundRequest() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function () {
+        document.getElementById("output").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "./pages/soundrequest.txt")
+    xhttp.send();
+}
+
+function loadGallery() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function () {
+        document.getElementById("output").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "./pages/gallery.txt")
+    xhttp.send();
+}
+
+// About category
+
+function loadStaff() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function () {
+        document.getElementById("output").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "./pages/staff.txt");
+    xhttp.send();
+}
+function loadHistory() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function () {
+        document.getElementById("output").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "./pages/history.txt")
+    xhttp.send();
+}
+
+function loadContact() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function () {
+        document.getElementById("output").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "./pages/contact.txt")
+    xhttp.send();
+}
+
+
---
+++ js/posterclick.js
@@ -0,0 +1,1 @@
+// I want this to listen to clicks on a poster, and if so, list the poster info side by side
