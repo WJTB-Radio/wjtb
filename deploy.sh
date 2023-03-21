248b225233c1cc39c5a76762d3be9da3f959a372
= <jul204040204625@gmail.com>
2023-03-21 15:08:04 -0400

    fix for real this time

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+# without this, we will delete the database whenever we deploy
+cat ./api/database.sqlite3 > ../api/public/database.sqlite3
+
 git fetch --depth 1
 git reset --hard origin/master
 git clean -dfx
 ./create_pages.sh
+
+# load the saved database
+cat ../api/public/database.sqlite3 > ./api/database.sqlite3
+
+# remove the temporary file
+rm ../api/public/database.sqlite3
